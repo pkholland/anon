d6cc33dc72a440ebe6c08d9f79e3ac7f96388f6d
Paul Holland <pholland@adobe.com>
2015-01-21 11:19:31 -0800

    tweaks to secrets cert generator

--- secrets.mk
+++ secrets.mk
@@ -3,7 +3,59 @@
 # as well as PEM cert files with those keys, as well as some
 # password values.  Any time that the rules of this makefile
 # _produce_ these values, they will be automatically generated
-# based on random values.
+# based on random values.  Several of the default behaviors are
+# specified in the files ca_config and cert_config for generating
+# root certificate and a server certificate, signed by that root.
+# One of the things this set of make rules produces is the file
+# certs/trusted_roots.pem.  If you import that pem file into the
+# set of trusted certificate authorities that your web browser understands,
+# you can then use the secrets/anon_cert.pem and secrets/anon_key.pem
+# files in an openssl-based server session, allowing your browser
+# to go to "https://localhost:<whatever port number>" and be able
+# to validate the connection and certificate chain.  See the
+# "epoxy" and "teflon" example projects in anon for an example
+# of how to do this.
+#
+# Example recipe:
+#
+#   1) cd anon
+#   2) make
+#   3) ./deploy/release/epoxy 1768 teflon
+#
+# with that running, in a second terminal window:
+#
+#   4) cd anon/deploy/release
+#   5) echo "start teflon" > .epoxy_cmd && cat .epoxy_cmd
+#
+# At this point the teflon web server should be running https on port 1768
+#
+# To verify directly with openssl do:
+#
+#   6) openssl s_client -connect localhost:1768 -CAfile ../../certs/trusted_roots.pem
+#
+# The last line of openssl's output should look like:
+#
+#     Verify return code: 0 (ok)
+#
+# To verify with Firefox do:
+#
+#   a) launch firefox
+#   b) select the menu File > Preferences
+#   c) select the "Advanced" tab
+#   d) click the "View Certificates" button
+#   e) click the "Import..." button
+#   f) navigate to anon/certs and select the trusted_roots.pem file
+#   g) click "Open"
+#   h) select the "Trust this CA to identify websites" check box
+#   i) click "OK"
+#   j) click "OK"
+#
+# then in firefox navitage to https://localhost:1768.  If all goes well
+# it will bring up the teflon page without any security complaints.
+#
+# To remove the certificate from Firefox's trusted list, search the
+# certificate view window for "ANON Open Development" and Delete its
+# "localhost" certificate.
 
 print_secrets=@printf "%-10s %-25s %s\n" "creating" $(1): $(2)
 
@@ -27,28 +79,28 @@ secrets/ca_cert.pem: secrets/ca_key.pem
 	
 
 
-secrets/raw_ims_password: | secrets
-	$(call print_secrets,"random ims password",$@)
-	@od --read-bytes 12 -vt x1 -An /dev/urandom | tr -d ' ' | tr -d '\n' > secrets/raw_ims_password
+secrets/raw_anon_password: | secrets
+	$(call print_secrets,"random anon password",$@)
+	@od --read-bytes 12 -vt x1 -An /dev/urandom | tr -d ' ' | tr -d '\n' > secrets/raw_anon_password
 
-secrets/ims_key.pem: secrets/raw_ims_password
-	$(call print_secrets,"ims private key",$@)
-	@openssl genrsa -passout pass:`cat secrets/raw_ims_password` -out secrets/ims_key.pem 2048 2> /dev/null
+secrets/anon_key.pem: secrets/raw_anon_password
+	$(call print_secrets,"anon private key",$@)
+	@openssl genrsa -passout pass:`cat secrets/raw_anon_password` -out secrets/anon_key.pem 2048 2> /dev/null
 
 # generate the request (openssl req) and then sign it with our root
 # (openssl ca) but, because the ca tool is intended to maintain a database
 # of previously signed requests, we blow away any accidentally left over
 # dirs and files from that.
-secrets/ims_cert.pem: secrets/ca_cert.pem secrets/ims_key.pem
-	$(call print_secrets,"ims cert request",$@)
-	@rm -f secrets/ims_cert.csr
+secrets/anon_cert.pem: secrets/ca_cert.pem secrets/anon_key.pem
+	$(call print_secrets,"anon cert request",$@)
+	@rm -f secrets/anon_cert.csr
 	@rm -rf obj/auto_gen/certs
-	@openssl req -new -key secrets/ims_key.pem -out secrets/ims_cert.csr -config cert_info/cert_config -batch
+	@openssl req -new -key secrets/anon_key.pem -out secrets/anon_cert.csr -config cert_info/cert_config -batch
 	@mkdir -p obj/auto_gen/certs/newcerts
-	@echo 00 > obj/auto_gen/certs/serial
+	@bash -c "printf \"%x\n\" \$$((\$$RANDOM%256))" > obj/auto_gen/certs/serial
 	@touch obj/auto_gen/certs/index.txt
-	@openssl ca -in secrets/ims_cert.csr -out secrets/ims_cert.pem -config cert_info/ca_config -notext -batch 2> /dev/null
-	@rm secrets/ims_cert.csr
+	@openssl ca -in secrets/anon_cert.csr -out secrets/anon_cert.pem -config cert_info/ca_config -notext -batch 2> /dev/null
+	@rm secrets/anon_cert.csr
 	@rm -rf obj/auto_gen/certs
 	
 	
@@ -57,19 +109,19 @@ certs/trusted_roots.pem: secrets/ca_cert.pem | certs
 	@cp -f secrets/ca_cert.pem certs/trusted_roots.pem
 	
 
-secrets/passwords.h: secrets/raw_ims_password secrets/raw_ims_password
+secrets/passwords.h: secrets/raw_anon_password secrets/raw_anon_password
 	$(call print_secrets,"c++ password .h file",$@)
 	@echo "#pragma once" > secrets/passwords.h
-	@printf "#define IMS_ROOT_CERT_PASSWORD \"" >> secrets/passwords.h
-	@cat secrets/raw_ims_password >> secrets/passwords.h
+	@printf "#define ANON_ROOT_CERT_PASSWORD \"" >> secrets/passwords.h
+	@cat secrets/raw_anon_password >> secrets/passwords.h
 	@printf "\"\n" >> secrets/passwords.h
-	@printf "#define IMS_CA_PASSWORD \"" >> secrets/passwords.h
-	@cat secrets/raw_ims_password >> secrets/passwords.h
+	@printf "#define ANON_CA_PASSWORD \"" >> secrets/passwords.h
+	@cat secrets/raw_anon_password >> secrets/passwords.h
 	@printf "\"\n" >> secrets/passwords.h
 	
 SECRET_FILES=\
 secrets/passwords.h\
-secrets/ims_cert.pem\
+secrets/anon_cert.pem\
 secrets/ca_cert.pem\
 certs/trusted_roots.pem
 
