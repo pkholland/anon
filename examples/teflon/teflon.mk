86dd13c8ca61343e61136602476968a37439c27e
Paul Holland <pholland@adobe.com>
2015-02-17 17:29:14 -0800

    improving the way ANON_PARENT is computed in makefiles

--- examples/teflon/teflon.mk
+++ examples/teflon/teflon.mk
@@ -4,6 +4,7 @@ parent_dir=$(patsubst %/,%,$(dir $(patsubst %.,%../foo,$(patsubst %..,%../../foo
 THIS_MAKE := $(lastword $(MAKEFILE_LIST))
 ANON_ROOT := $(call parent_dir,$(call parent_dir,$(call parent_dir,$(THIS_MAKE))))
 ANON_PARENT := $(patsubst ./..%,..%,$(ANON_ROOT)/..)
+ANON_PARENT := $(patsubst ../anon/..,..,$(ANON_PARENT))
 
 SOURCES+=\
 $(ANON_ROOT)/examples/teflon/main.cpp\
