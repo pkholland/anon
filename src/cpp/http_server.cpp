39160f8fbca95de05c05247cf278c6eea83c1a0a
Paul Holland <pkholland63@gmail.com>
2019-08-09 13:53:28 -0700

    adding debug logging

--- src/cpp/http_server.cpp
+++ src/cpp/http_server.cpp
@@ -184,7 +184,9 @@ void http_server::start_(int tcp_port, body_handler *base_handler, int listen_ba
           //anon_log("client sent:\n" << &buf[0] << "\n\n");
 
           // call the joyent parser
+          auto orig_bsp = bsp;
           bsp += http_parser_execute(&parser, &settings, &buf[bsp], bep - bsp);
+          anon_log("bsp stated: " << orig_bsp << ", bep: " << bep << ", new bsp: " << bsp);
 
           if (pcallback.message_complete)
           {
@@ -227,6 +229,7 @@ void http_server::start_(int tcp_port, body_handler *base_handler, int listen_ba
 #endif
             if (keep_alive)
             {
+              anon_log("keep_alive moving data to start of buffer, bsp: " << bsp << ", bep: " << bep);
               http_parser_init(&parser, HTTP_REQUEST);
               memmove(&buf[0], &buf[bsp], bep - bsp);
               bep -= bsp;
