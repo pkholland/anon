22e8851098fda52567fd9ad631e388bd5cadcee9
Paul Holland <Paul Holland>
2021-10-20 09:30:38 -0700

    removing logging that doesn't work

--- src/cpp/sproc_mgr.cpp
+++ src/cpp/sproc_mgr.cpp
@@ -197,8 +197,6 @@ int start_child(proc_info &pi)
       kill(pid, SIGKILL);
       throw std::runtime_error("child process failed to start correctly");
     }
-    auto msg = "from parent, child signaled it is running\n";
-    if (write(1, msg, strlen(msg)));
   }
   else
   {
@@ -260,9 +258,6 @@ int start_child(proc_info &pi)
     for (auto &e : pi.envs_)
       envs2.push_back((char*)e.c_str());
     envs2.push_back(0);
-
-    auto msg = "from child, last log prior to fexecve to child code\n";
-    if (write(1, msg, strlen(msg)));
 
     fexecve(pi.exe_fd_, &args2[0], &envs2[0]);
 
@@ -392,18 +387,8 @@ void sproc_mgr_init(int port, const std::vector<int> udp_ports, bool udp_is_ipv6
 
           std::unique_lock<std::mutex> lock(proc_map_mutex);
           auto p = proc_map.find(chld);
-          if (p == proc_map.end()) {
+          if (p == proc_map.end())
             anon_log("ignoring unregistered child process id: " << chld);
-            std::ostringstream path;
-            path << "/proc/" << chld << "/cmdline";
-            auto fd = ::open(path.str().c_str(), O_RDONLY);
-            if (fd != -1) {
-              std::vector<char> cmd_buff(4096);
-              auto len = ::read(fd, &cmd_buff[0], cmd_buff.size());
-              anon_log("-> " << std::string(&cmd_buff[0], len));
-              ::close(fd);
-            }
-          }
           else
           {
             auto pi = std::move(p->second);
