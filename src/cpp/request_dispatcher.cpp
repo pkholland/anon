9e280b5c5e5bcc954d9b3b54eea84a6fa8add161
Paul Holland <pkholland63@gmail.com>
2023-07-14 16:22:54 -0700

    add request headers to OPTIONS response

--- src/cpp/request_dispatcher.cpp
+++ src/cpp/request_dispatcher.cpp
@@ -178,6 +178,9 @@ void respond_options(http_server::pipe_t &pipe, const http_request &request, con
   auto orig2 = orig.len() > 0 ? orig.str() : std::string("*");
   response.add_header("access-control-allow-origin", orig2);
   response.add_header("access-control-allow-methods", request.headers.get_header("access-control-request-method").str());
+  if (request.headers.contains_header("access-control-request-headers")) {
+    response.add_header("access-control-allow-headers", request.headers.get_header("access-control-request-headers").str());
+  }
   response.add_header("access-control-max-age", "600");
   if (allowed_headers.size() > 0) {
     std::ostringstream oss;
